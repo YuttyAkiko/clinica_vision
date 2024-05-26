9fc1ac3a082718183c4051605f9fc1633abff967
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2024-05-26 20:57:50 -0300

    feature: adicionado estilo aos fields do formulario de atualizar_dados

--- medicos/forms.py
+++ medicos/forms.py
@@ -7,3 +7,14 @@ class Update_Medico_Form(forms.ModelForm):
         fields = ('nome','sobrenome','genero','email',
                   'crm','telefone','especialidade')
         
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['nome'].widget.attrs['readonly'] = True
+        self.fields['sobrenome'].widget.attrs['readonly'] = True
+        self.fields['genero'].widget.attrs['readonly'] = True
+        
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input-estilizado'})
+            if field_name not in ['genero','especialidade','crm','telefone']:
+                field.widget.attrs.update({'maxlength': '30', 'size': '40'})
+            
