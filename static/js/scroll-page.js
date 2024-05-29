edf0d1e5d5ed15b8213569d1d4353ba44b22e4a9
Yutty Akiko <yutty.akiko@gmail.com>
2024-05-29 11:55:21 -0300

    feature: Adicionei sessão de contato na home page. fix: Ajustei o erro que tava dando no link login do header.

--- static/js/scroll-page.js
+++ static/js/scroll-page.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const links = document.querySelectorAll('nav a');
-  const offset = 100; // Ajuste o valor conforme necessário
+    const links = document.querySelectorAll('nav a');
+    const offset = 100; // Ajuste o valor conforme necessário
 
-  links.forEach(link => {
-      link.addEventListener('click', function (event) {
-          event.preventDefault();
-          const targetId = this.getAttribute('href').substring(1);
-          const targetElement = document.getElementById(targetId);
+    links.forEach(link => {
+        link.addEventListener('click', function (event) {
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
 
-          if (targetElement) {
-              const elementPosition = targetElement.getBoundingClientRect().top;
-              const offsetPosition = elementPosition + window.pageYOffset - offset;
+            if (targetElement) {
+                event.preventDefault();
+                const elementPosition = targetElement.getBoundingClientRect().top;
+                const offsetPosition = elementPosition + window.pageYOffset - offset;
 
-              window.scrollTo({
-                  top: offsetPosition,
-                  behavior: 'smooth'
-              });
-          }
-      });
-  });
+                window.scrollTo({
+                    top: offsetPosition,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
 });
